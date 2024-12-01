0211fcac845748e98c736a3bf52f6d25839b8831
Алексей Попов <95385068+kvufee@users.noreply.github.com>
2024-12-01 05:14:40 +0300

    [ADD] add service model

--- src/service/ai/service.py
+++ src/service/ai/service.py
@@ -1,1 +1,52 @@
-# полный цикл обучения и тд всякой (предсказать)
+import numpy as np
+import pickle
+
+from src.dto.staff_public_dto import StaffPublicDTO
+from sklearn.neighbors import NearestNeighbors
+from sklearn.preprocessing import OneHotEncoder
+from typing import List
+
+
+class NearestNeighborsModel:
+    def __init__(self, data: List[StaffPublicDTO], n_neighbors=10):
+        self.data = data
+        self.n_neighbors = n_neighbors
+        self.encoder = OneHotEncoder(handle_unknown='ignore')
+        self.nn = NearestNeighbors(n_neighbors=self.n_neighbors, metric='euclidean')
+        self.encoded_data = None
+        self.columns = self.get_columns()
+
+    def get_columns(self):
+        return [field.name for field in StaffPublicDTO.__dataclass_fields__.values()]
+
+    def fit(self):
+        data_matrix = [[getattr(row, col) for col in self.columns] for row in self.data]
+        self.encoded_data = self.encoder.fit_transform(data_matrix).toarray()
+        self.nn.fit(self.encoded_data)
+
+    def find_nearest_neighbors(self, input_data: StaffPublicDTO):
+        input_data = [getattr(input_data, col, 'None') for col in self.columns]
+        encoded_input_data = self.encoder.transform([input_data]).toarray()
+        indices = self.nn.kneighbors(encoded_input_data)
+        nearest_neighbors = [self.data[index] for index in indices[0]]
+        return nearest_neighbors
+
+    def add_data(self, new_data: List[StaffPublicDTO]):
+        self.data.extend(new_data)
+        self.fit()
+
+    def save_model(self, file_path):
+        with open(file_path, 'wb') as file:
+            pickle.dump(self, file)
+
+    @staticmethod
+    def load_model(file_path):
+        with open(file_path, 'rb') as file:
+            return pickle.load(file)
+
+
+model = NearestNeighborsModel(data)
+model.fit()
+
+nearest_neighbors = model.find_nearest_neighbors(input_data)
+print(nearest_neighbors)
